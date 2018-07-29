f8693e6600f8ca554d5dc68cdf1fb0ffbd02112e
heltonsantacruz <heltonsantacruz@gmail.com>
2018-07-29 14:19:49 -0300

    Ajuste da pasta de documentação

---
+++ Documentação/Exercicios/Heurísticas.mm
@@ -0,0 +1,85 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1532778377459" ID="ID_1116146058" MODIFIED="1532884517545">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Exerc&#237;cio<br />Heur&#237;sticas
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1532778701375" ID="ID_636666084" MODIFIED="1532778705752" POSITION="right" TEXT="Produto Similar">
+<node CREATED="1532778392238" ID="ID_388760171" MODIFIED="1532884660104" TEXT="DOE Alagoas">
+<node CREATED="1532778426718" ID="ID_1950974337" MODIFIED="1532884484357" TEXT="Jacon Nielsen">
+<node CREATED="1532884524172" ID="ID_1883247073" MODIFIED="1532884527464" TEXT="Heur&#xed;sticas">
+<node CREATED="1532778494537" ID="ID_586501666" MODIFIED="1532778495032" TEXT="1) Feedback">
+<node CREATED="1532778516835" ID="ID_1631627615" MODIFIED="1532780748012">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      O sistema se comportou, de modo geral, popsitivamente. Apresenta uma interface clara e objetiva, informando de forma precisa e clara todos os passos do usu&#225;rio ao utilizar as funcionalidades. Alguns pontos que poderiam ser melhorados s&#227;o com rela&#231;&#227;o as cores de alguns bot&#245;es que n&#227;o seguem o padr&#227;o de uso normal, e algumas figuras utilizadas que n&#227;o condizem com a funcionalidade esperada, como a figura de compartilhamento do di&#225;rio, que exibe a figura padr&#227;o de gera&#231;&#227;o de arquivo pdf, por&#233;m, ao acionar a funcionalidade, &#233; compartilhado apenas um print da tela e n&#227;o o di&#225;rio pesquisado em formato &quot;pdf&quot;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1532778499046" ID="ID_685881862" MODIFIED="1532778499372" TEXT="2) Falar a linguagem do usu&#xe1;rio">
+<node CREATED="1532781237394" ID="ID_1451334667" MODIFIED="1532781677645">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A linguagem do sistema est&#225; equivalente e conforme&#160;o perfil de usu&#225;rio que ir&#225; utiliz&#225;-lo n&#227;o demonstrando nenhum tipo de termo ou nomenclatura que possam constrastar com o esperado.&#160;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1532778503358" ID="ID_1969287741" MODIFIED="1532778503722" TEXT="3) Controles e liberdade">
+<node CREATED="1532785990279" ID="ID_1484358202" MODIFIED="1532786371613">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      O sistema &#233; voltado para consulta de informa&#231;&#245;es e n&#227;o est&#225; direcionado para grava&#231;&#227;o de dados que possam ser utilizados para posteriores consultas. Por&#233;m, as funcionalidades disponibilizadas e testadas permitem ao usu&#225;rio voltar a situa&#231;&#245;es anteriores com facilidade como entrar na funcionalidade de bate papo ap&#243;s realizar v&#225;rias consultas por di&#225;rios
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1532778658775" ID="ID_33329927" MODIFIED="1532778659584" TEXT="4) Consist&#xea;ncia e padr&#xf5;es"/>
+<node CREATED="1532778664087" ID="ID_452051313" MODIFIED="1532778664410" TEXT="5) Preven&#xe7;&#xe3;o de erros"/>
+<node CREATED="1532778668340" ID="ID_1877427194" MODIFIED="1532778668683" TEXT="6) Reconhecimento"/>
+<node CREATED="1532778672188" ID="ID_1429662216" MODIFIED="1532778672521" TEXT="7) Atalhos"/>
+<node CREATED="1532778676614" ID="ID_934355921" MODIFIED="1532778676920" TEXT="8) Di&#xe1;logos simples"/>
+<node CREATED="1532778680106" ID="ID_709454459" MODIFIED="1532778680828" TEXT="9) Reconhecimento, diagn&#xf3;stico e recupera&#xe7;&#xe3;o"/>
+<node CREATED="1532778684973" ID="ID_842810585" MODIFIED="1532778685679" TEXT="10) Ajuda e documenta&#xe7;&#xe3;o"/>
+</node>
+</node>
+<node CREATED="1532884485925" ID="ID_604272356" MODIFIED="1532884500335">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      MICHAEL BOLTON<br />(TESTING WITHOUT A MAP)
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1532884554348" ID="ID_733522887" MODIFIED="1532884558560" TEXT="Heur&#xed;sticas"/>
+</node>
+</node>
+</node>
+</node>
+</map>
