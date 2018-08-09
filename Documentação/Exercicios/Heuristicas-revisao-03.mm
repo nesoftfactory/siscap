450cf28204078c4da017355c0b92301d7358509b
AGAmorim <ag.amorim.ce@gmail.com>
2018-08-09 06:06:27 -0300

    WBS

---
+++ Documentação/Exercicios/Heuristicas-revisao-03.mm
@@ -0,0 +1,71 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1533261011574" ID="7tgqcvebq7dm0ts37ck10qdq83" MODIFIED="1533261011574" TEXT="Exerc&#xed;cioHeur&#xed;sticas">
+<node CREATED="1533261011575" ID="1lmp1qcmg6euauqu24a0l7s8q2" MODIFIED="1533261011575" POSITION="right" TEXT="Produto Similar">
+<node CREATED="1533261011575" ID="4d0i38hn0l20en5n2ubk448guj" MODIFIED="1533261011575" TEXT="DOE Alagoas">
+<node CREATED="1533261011575" ID="394127i4ktvfdgm8mlbm94jrvv" MODIFIED="1533261011575" TEXT="Jacob Nielsen">
+<node CREATED="1533261011575" ID="2g08uptducptgi24gg5fa52ki8" MODIFIED="1533261011575" TEXT="Heur&#xed;sticas">
+<node CREATED="1533261011575" ID="05c9l4aplg79ahc841c1cag12l" MODIFIED="1533602641925" TEXT="1) Feedback">
+<node CREATED="1533261011575" HGAP="24" ID="6ot0sn0n8k0hebklkfk62gcj9c" MODIFIED="1533602865082" STYLE="bubble" TEXT="O sistema se comportou, de modo geral, positivamente. Apresenta uma interface clara e objetiva, informando de forma precisa, todos os passos do usu&#xe1;rio ao utilizar as funcionalidades. Alguns pontos que poderiam ser melhorados s&#xe3;o com rela&#xe7;&#xe3;o as cores de alguns bot&#xf5;es que n&#xe3;o seguem o padr&#xe3;o de uso normal, e algumas figuras utilizadas que n&#xe3;o condizem com a funcionalidade esperada, como a figura de compartilhamento do di&#xe1;rio, que exibe a figura padr&#xe3;o de gera&#xe7;&#xe3;o de arquivo pdf, por&#xe9;m, ao acionar a funcionalidade, &#xe9; compartilhado apenas um print da tela e n&#xe3;o o di&#xe1;rio pesquisado em formato &quot;pdf&quot;." VSHIFT="-4"/>
+</node>
+<node CREATED="1533261011575" ID="1pmlrft0s3478g4602n90unftp" MODIFIED="1533261011575" TEXT="2) Falar a linguagem do usu&#xe1;rio">
+<node CREATED="1533261011575" ID="4d0c7pjb51vn5vn8pb07f4h2ht" MODIFIED="1533602877573" STYLE="bubble" TEXT="A linguagem do sistema est&#xe1; equivalente e conforme&#xa0;o perfil de usu&#xe1;rio que ir&#xe1; utiliz&#xe1;-lo n&#xe3;o demonstrando nenhum tipo de termo ou nomenclatura que possam constrastar com o esperado.&#xa0;"/>
+</node>
+<node CREATED="1533261011575" ID="24215j66j6cgrfn6m0akpaq7i6" MODIFIED="1533261011575" TEXT="3) Controles e liberdade">
+<node CREATED="1533261011575" ID="1bgv3i8hnqj5ahlrr8bk3khih8" MODIFIED="1533602899156" STYLE="bubble" TEXT="O sistema &#xe9; voltado para consulta de informa&#xe7;&#xf5;es e n&#xe3;o est&#xe1; direcionado para grava&#xe7;&#xe3;o de dados que possam ser utilizados para posteriores consultas. Por&#xe9;m, as funcionalidades disponibilizadas e testadas permitem ao usu&#xe1;rio voltar a situa&#xe7;&#xf5;es anteriores com facilidade como entrar na funcionalidade de bate papo ap&#xf3;s realizar v&#xe1;rias consultas por di&#xe1;rios."/>
+</node>
+<node CREATED="1533261011575" ID="7u2fbg1o207b3u1u79uar76af6" MODIFIED="1533261011575" TEXT="4) Consist&#xea;ncia e padr&#xf5;es">
+<node CREATED="1533261011575" ID="4ofual6cchn7apfgs2ckih9s8j" MODIFIED="1533602911704" STYLE="bubble" TEXT="O sistema obteve alguns elementos que fugiram do padr&#xe3;o, h&#xe1; alguns pontos a serem considerados como melhoria:&#xa;1) Cada di&#xe1;rio oficial possui logo abaixo a abrevia&#xe7;&#xe3;o &quot;DOE&quot; seguida da data, entretanto somente o di&#xe1;rio oficial do dia possui um texto que indica m&#xea;s e ano que este di&#xe1;rio pertence.&#xa;2) H&#xe1; linhas que dividem a p&#xe1;gina, tais como uma linha entre o menu superior e o di&#xe1;rio oficial do dia, outra linha entre o di&#xe1;rio oficial do dia e os di&#xe1;rios anteriores, e ainda outra linha entre os di&#xe1;rios anteriores e o menu de pagina&#xe7;&#xe3;o. H&#xe1; uma cor de linha que foge do padr&#xe3;o, esta divide o di&#xe1;rio oficial do dia e os di&#xe1;rios anteriores.&#xa;3) O Bot&#xe3;o Buscar logo abaixo da caixa &quot;select option&quot; possui comportamento diferente do menu superior e do menu de pagina&#xe7;&#xe3;o. Este &#xe9; um bot&#xe3;o HTML simples, n&#xe3;o possui comportamento ao passar o mouse por cima, e comportamento de clicar, tamb&#xe9;m difere dos componentes clic&#xe1;veis nos menus.&#xa;4) A &#xe1;rea destinada a edi&#xe7;&#xe3;o do dia est&#xe1; centralizada, enquanto a &#xe1;rea dos di&#xe1;rios anteriores est&#xe1; ajustada a margem esquerda.&#xa;&#xa;Foi encontrado ainda um erro grav&#xed;ssimo, ao clicar no ano de 2016 n&#xe3;o h&#xe1; imagem dos di&#xe1;rios oficiais, conforme &#xe9; visto ao clicar nos anos de 2017 e 2018."/>
+</node>
+<node CREATED="1533261011575" ID="21t09rs5a9rd7jv8sg9s6q9iac" MODIFIED="1533261011575" TEXT="5) Preven&#xe7;&#xe3;o de erros">
+<node CREATED="1533261011575" ID="5djj7kc64g2gqna7j9k0fh535n" MODIFIED="1533602926788" STYLE="bubble" TEXT="O sistema falha completamente neste ponto por n&#xe3;o permitir ao usu&#xe1;rio desfazer a&#xe7;&#xf5;es indesejadas pelos seguintes motivos:&#xd;&#xa;1) Ao clicar em um di&#xe1;rio oficial qualquer, n&#xe3;o h&#xe1; a op&#xe7;&#xe3;o de voltar para p&#xe1;gina anterior.&#xd;&#xa;2) A p&#xe1;gina de contato n&#xe3;o exibe di&#xe1;logo de confirma&#xe7;&#xe3;o antes de enviar a mensagem."/>
+</node>
+<node CREATED="1533261011575" ID="2f98hu83pi9l4c7ojt5dstvg38" MODIFIED="1533261011575" TEXT="Reconhecimento ao inv&#xe9;s de Lembran&#xe7;a">
+<node CREATED="1533261011575" ID="4jk04pg2u4mmhk8t540tc97qvo" MODIFIED="1533602933685" STYLE="bubble" TEXT="O sistema apresenta facilidade de navega&#xe7;&#xe3;o, o menu superior e o menu de pagina&#xe7;&#xe3;o s&#xe3;o bastante familiares. O Menu superior superior ainda possui links para p&#xe1;ginas bastante conhecidas, tais como p&#xe1;gina institucional, contato e p&#xe1;ginas bem espec&#xed;ficas deste segmento de website."/>
+</node>
+<node CREATED="1533261011575" ID="5ig4t0nga2uo50fr76tdgidodg" MODIFIED="1533261011575" TEXT="7) Atalhos">
+<node CREATED="1533261011575" ID="1011t8o8jep04p3ed7av47phng" MODIFIED="1533602953489" STYLE="bubble" TEXT="Sob essa perspectiva foram observados os seguintes pontos negativos:&#xd;&#xa;1) Di&#xe1;rio Oficial ao ser clicado, n&#xe3;o &#xe9; redirecionado para o PDF, e sim para uma p&#xe1;gina que d&#xe1; a op&#xe7;&#xe3;o de visualizar o PDF completo&#xd;&#xa;2) N&#xe3;o foram observadas nenhuma m&#xe1;scara no formul&#xe1;rio de contato&#xd;&#xd;&#xa;Foi poss&#xed;vel utilizar sem nenhum problema os atalhos de teclado &quot;tab&quot; e &quot;enter&quot; para navegar na tela, a dificuldade foi visualizar o elemento selecionado.&#xd;&#xa;"/>
+</node>
+<node CREATED="1533261011575" ID="5lmgal5p1peaamlnbtq77mpc2r" MODIFIED="1533261011575" TEXT="8) Di&#xe1;logos simples">
+<node CREATED="1533261011575" ID="1ohc3b2f0qh3vg7k2endagld1d" MODIFIED="1533602978634" STYLE="bubble" TEXT="O sistema apresenta dialogo simples conforme a Heur&#xed;sitica de Nielsen, uma vez que facilita a busca do usu&#xe1;rio que deseja encontrar um di&#xe1;rio oficial espec&#xed;fico de um m&#xea;s. O site apresenta na p&#xe1;gina inicial, uma caixa de &quot;select option&quot; com os meses em que h&#xe1; um di&#xe1;rio, seguido de um bot&#xe3;o buscar logo abaixo. "/>
+</node>
+<node CREATED="1533261011575" ID="62lb4n87vnv57t30t8eonc8a32" MODIFIED="1533261011575" TEXT="9) Reconhecimento, diagn&#xf3;stico e recupera&#xe7;&#xe3;o">
+<node CREATED="1533261011575" ID="0s54ilbik3ulm6vnkbsn7lfva3" MODIFIED="1533602988381" STYLE="bubble" TEXT="O sistema se comportou adequadamente ao alertar o usu&#xe1;rio sobre erros de entrada de dados. O Mesmo sinalizou de maneira satisfat&#xf3;ria os campos digitados err&#xf4;neamente e explicou como deveria digitar na p&#xe1;gina de contato.&#xd;&#xa;"/>
+</node>
+<node CREATED="1533261011575" ID="6sbsnjuds5souje6r75ahdabn5" MODIFIED="1533602626495" TEXT="10) Ajuda e documenta&#xe7;&#xe3;o">
+<node CREATED="1533261011575" ID="18348auk4amb6i8n67l51mvln9" MODIFIED="1533602994006" STYLE="bubble" TEXT="O sistema n&#xe3;o apresenta a op&#xe7;&#xe3;o de ajuda em seu menu, ou a sess&#xe3;o de perguntas mais frequentes em seu site o que dificultou bastante minha pesquisa por ajuda. O mais pr&#xf3;ximo que h&#xe1; para resolver minha d&#xfa;vida &#xe9; um campo de busca no menu superior ou a p&#xe1;gina de contato."/>
+</node>
+</node>
+</node>
+<node CREATED="1533261011575" ID="4s12asv1jjlgtad6baaqnpk0rl" MODIFIED="1533261011575" TEXT="MICHAEL BOLTON(TESTING WITHOUT A MAP)">
+<node CREATED="1533261011576" ID="3v2jp7tamgneg9m6ba6ris0m6d" MODIFIED="1533261011576" TEXT="Heur&#xed;sticas">
+<node CREATED="1533261011576" ID="5q9b21ut0m7mb0jntvbpn4pktk" MODIFIED="1533261011576" TEXT="Hist&#xf3;rico">
+<node CREATED="1533261011576" ID="7orjg47fgp8b0iekeecti7cv5j" MODIFIED="1533603010435" STYLE="bubble" TEXT="O sistema possui funcionalidades semelhantes a outros sites que possuem mesma funcionalidade, h&#xe1; lista de di&#xe1;rios oficiais, pagina&#xe7;&#xe3;o e ao clicar em um di&#xe1;rio oficial &#xe9; poss&#xed;vel visualizar o PDF. Portanto, o site estudado cumpre esta heur&#xed;stica."/>
+</node>
+<node CREATED="1533261011576" ID="4mvsidrnrjhrankmiqjoaofgra" MODIFIED="1533261011576" TEXT="Imagem">
+<node CREATED="1533261011576" ID="1jlaf0jqrdhbk3bcmga3a4q96p" MODIFIED="1533603016966" STYLE="bubble" TEXT="O sistema cumpre esta heur&#xed;stica! Foi observado que o site, assim como outros do mesmo segmento possui caracter&#xed;sticas semelhantes, tais como p&#xe1;gina clean, uso de tipografia t&#xed;pica para sites do ramo jur&#xed;dico, logo do &#xf3;rg&#xe3;o, uso maior de textos em detrimento a imagens, todos estes elementos passam a imagem s&#xf3;bria e s&#xe9;ria de um &#xf3;rg&#xe3;o p&#xfa;blico. "/>
+</node>
+<node CREATED="1533261011576" ID="5v0jn2enp3sqcnqd7sbahlg0jb" MODIFIED="1533261011576" TEXT="Produtos Compar&#xe1;veis">
+<node CREATED="1533261011576" ID="4bva80f56l461kdu5ii88oudf1" MODIFIED="1533603023472" STYLE="bubble" TEXT="H&#xe1; para esta heur&#xed;stica sites de listagem de di&#xe1;rios oficiais semelhantes ao DOE Alagoas, tais como o Di&#xe1;rio Oficial do PI (http://www.diariooficial.pi.gov.br/) ou Di&#xe1;rio Oficial do Tocantins (https://diariooficial.to.gov.br/). Todos eles possuem o objetivo de listar di&#xe1;rios oficiais de seus respectivos estados."/>
+</node>
+<node CREATED="1533261011576" ID="3pohh9oa4hlajvufdio73q5lb2" MODIFIED="1533261011576" TEXT="Exig&#xea;ncias">
+<node CREATED="1533261011576" ID="391u3e4r82qndjn4gjnqrr8tpt" MODIFIED="1533603033097" STYLE="bubble" TEXT="A principal funcionalidade do produto &#xe9; disponibilizar di&#xe1;rios oficiais para download e leitura. Esta &#xe9; claramente cumprida."/>
+</node>
+<node CREATED="1533261011576" ID="0kkvsv9nd27fijdlmn4isnhb14" MODIFIED="1533261011576" TEXT="Expectativas do Usu&#xe1;rio">
+<node CREATED="1533261011576" ID="1llv8oh0ednjrlcnrum9nqa3tv" MODIFIED="1533603040380" STYLE="bubble" TEXT="O sistema cumpre a expectativa do usu&#xe1;rio, uma vez que disponibiliza di&#xe1;rios oficiais do estado para leitura e download."/>
+</node>
+<node CREATED="1533261011576" ID="1sc8k90lnmkndhlf3skugob5ub" MODIFIED="1533261011576" TEXT="O pr&#xf3;prio produto">
+<node CREATED="1533261011576" ID="75dt3amcvgqckug38m06art8h9" MODIFIED="1533603050174" STYLE="bubble" TEXT="A funcionalidade de leitura/download de di&#xe1;rios oficiais n&#xe3;o tem o mesmo comportamento se comparado a outros sites. O PDF n&#xe3;o &#xe9; visualizado imediantamente ao clicar no &#xed;cone de um Di&#xe1;rio Oficial como ocorre nos outros sites de mesma fun&#xe7;&#xe3;o. &#xc9; necess&#xe1;rio entrar na p&#xe1;gina daquele di&#xe1;rio e clicar em um link para abr&#xed;-lo."/>
+</node>
+<node CREATED="1533261011576" ID="4fd7at58m8gim160uiacsvatmp" MODIFIED="1533261011576" TEXT="Prop&#xf3;sito">
+<node CREATED="1533261011576" ID="6mirgeavu980c4scf88odmbj89" MODIFIED="1533603057568" STYLE="bubble" TEXT="A proposta do produto &#xe9; listar os di&#xe1;rios oficiais e disponibiliz&#xe1;-los para download e leitura. Esta proposta &#xe9; atendida."/>
+</node>
+<node CREATED="1533261011577" ID="3gagbv0ju9dj5v67h5oaoc52rp" MODIFIED="1533261011577" TEXT="Estatutos">
+<node CREATED="1533261011577" ID="6uk6fe11dj3vt1qgsoiam41tul" MODIFIED="1533603066323" STYLE="bubble" TEXT="O produto &#xe9; uma exig&#xea;ncia da pr&#xf3;pria lei, em que di&#xe1;rios oficiais estejam dispon&#xed;veis para consulta p&#xfa;blica, uma vez que &#xe9; fruto do pr&#xf3;prio &#xf3;rg&#xe3;o jur&#xed;dico, esta heur&#xed;stica &#xe9; cumprida."/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
