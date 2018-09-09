d0f675c861d85afc08b06fbc787de6280e309019
Antonio Filho <amsf10@gmail.com>
2018-09-08 22:07:21 -0300

    Somente para testar integracao com slack

--- src/API-REST/siscap-api/src/main/java/br/gov/pi/tce/siscap/api/repository/UsuarioRepository.java
+++ src/API-REST/siscap-api/src/main/java/br/gov/pi/tce/siscap/api/repository/UsuarioRepository.java
@@ -20,6 +20,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	Optional<Usuario> findByLogin(String login);
 	
-	@Query("SELECT u FROM Usuario u WHERE u.login = :login AND u.id != :idUsuario")
+	@Query("SELECT us FROM Usuario us WHERE us.login = :login AND us.id != :idUsuario")
 	List<Usuario> buscarPorLoginComIdDiferenteDoInformado(@Param("login") String login, @Param("idUsuario") Long idUsuario);
 }
