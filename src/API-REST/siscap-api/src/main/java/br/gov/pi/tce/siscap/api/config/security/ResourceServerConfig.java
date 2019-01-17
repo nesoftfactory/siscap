c2ce43e31901efa75edc9fcfad3dad2540586932
antonio.moreira <antonio.moreira@DIDES-13714.tce.pi.gov.br>
2019-01-16 23:00:55 -0300

    API: Obrigatoriedade de token para acessar o recurso fontes.

--- src/API-REST/siscap-api/src/main/java/br/gov/pi/tce/siscap/api/config/security/ResourceServerConfig.java
+++ src/API-REST/siscap-api/src/main/java/br/gov/pi/tce/siscap/api/config/security/ResourceServerConfig.java
@@ -24,7 +24,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/fontes").permitAll()
+				//.antMatchers("/fontes").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
